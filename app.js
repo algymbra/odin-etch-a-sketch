205baf4f715ea98b1efb9e9057f58d1b32ece506
Muhibbulloh <225215017+muhib137@users.noreply.github.com>
2025-08-15 15:54:21 +0500

    Set default grid size

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const gridContainer = document.querySelector(".container");
 const dropdownMenu = document.querySelector(".grid-size");
 const customInputEl = document.querySelector("input[type='number']");
 
+
+let gridSize = 32;
+
 dropdownMenu.addEventListener("change", (e) => {
   customInputEl.classList.add("hidden");
+
   if (e.target.value === "custom") {
     customInputEl.classList.remove("hidden");
+    gridSize = null;
+  } else {
+    gridSize = parseInt(e.target.value);
   }
+  
 });
 
 createBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(dropdownMenu.value);
+
+  gridSize =  gridSize || parseInt(customInputEl.value);
+
 });
